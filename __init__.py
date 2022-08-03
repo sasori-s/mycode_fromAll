259b8b9807a940c0c113a58b82b2ee6ee6152f55
Sams <shams.iqbal@g.bracu.ac.bd>
2022-08-03 12:42:17 +0600

    Fixed __name__ in __init__

--- __init__.py
+++ __init__.py
@@ -1,10 +1,9 @@
-from distutils.log import debug
-from pip import main
 from flask import Flask
-app = Flask( _name___)
-@app.route(' /')
-def hello_world() :
-    return ' Hello, World! '
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return ' Hello, World!'
 
 if __name__ == "__main__":
-    app.run(debug=True )
+    app.run(debug=True)
