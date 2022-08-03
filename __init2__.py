41632fc898b08719b1a92ea9ff4351f326e15b49
Sams <shams.iqbal@g.bracu.ac.bd>
2022-08-03 11:42:05 +0600

    Added a __init2__ file

---
+++ __init2__.py
@@ -0,0 +1,10 @@
+from distutils.log import debug
+from pip import main
+from flask import Flask
+app = Flask( _name___)
+@app.route(' /')
+def hello_world() :
+    return ' Hello, World! '
+
+if __name__ == "__main__":
+    app.run(debug=True)
